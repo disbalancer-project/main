f33c0b817ffc493c8151b823a3e6ffdd8917f030
disbalancer-project <100605261+disbalancer-project@users.noreply.github.com>
2022-03-19 15:21:35 +0100

    Linux autosetup script

---
+++ scripts/linux-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt install -y zip
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+cd /
+wget https://github.com/disbalancer-project/main/raw/main/launcher-disbalancer-docker-x64.zip
+unzip launcher-disbalancer-docker-x64.zip
+cd launcher-disbalancer-docker-x64
+docker build -t disbalancer .
+
+screen -S liberator -dm docker run disbalancer
+echo "Done!"
